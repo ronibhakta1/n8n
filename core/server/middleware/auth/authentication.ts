7a39b2200868394482eec281dc2dd7c1c6e3dc22
Turbobot <turbobot@vercel.com>
2025-09-10 21:21:43 +0530

    feat(create-turbo): apply official-starter transform

---
+++ core/server/middleware/auth/authentication.ts
@@ -0,0 +1,5 @@
+import { type Request ,type Response, type NextFunction } from 'express';
+
+const auth = (req: Request, res: Response, next: NextFunction) => {
+
+}
--- core/server/routes/v1/index.ts
+++ core/server/routes/v1/index.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 
-
 const router = Router();
-
 
 router.get('/', (req, res) => {
     res.send('API v1 is working');
